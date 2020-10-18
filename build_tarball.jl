a1db8bffe8239dff37bb79dc9e02ff2a97c8fbd7
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2020-10-19 02:46:56 +0900

    increase platforms

--- build_tarball.jl
+++ build_tarball.jl
@@ -17,8 +17,11 @@ install_license /usr/share/licenses/MIT
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [ 
-    MacOS(:x86_64)
+platforms = [
+    #Linux(:armv7l; libc=:glibc, compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
+    Windows(:x86_64; compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
+    Linux(:x86_64; libc=:glibc, compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
+    MacOS(:x86_64; compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
 ]
 
 # The products that we will ensure are always built
@@ -30,5 +33,5 @@ products = [
 dependencies = Dependency[
 ]
 
-# Build the tarballs.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version = v"7")
